a723b4185486effab7f2d34ee31c97d7d49733a7
aspcodenet <stefan.holmberg@systementor.se>
2023-02-16 13:02:25 +0100

    Add project files.

---
+++ GissaEttTal/App.cs
@@ -0,0 +1,19 @@
+﻿public class App
+{
+    public void Run()
+    {
+        var random = new Random();
+        Console.WriteLine("Gissa ett tal mellan 1 och 100.");
+        int secretNumber = random.Next(0,100);
+        Console.WriteLine(secretNumber);
+        while (true)
+        {
+            int tal = Convert.ToInt32(Console.ReadLine());
+            if (tal == secretNumber)
+                break;
+        }
+    }
+}
+
+
+
